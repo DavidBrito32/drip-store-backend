0ec31ca9b136e5402b39401e75fbbc664dccdbb7
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-12-17 19:46:01 -0300

    inclusão de comentarios

--- src/database/database_connection.ts
+++ src/database/database_connection.ts
@@ -3,9 +3,9 @@ import mysql2 from "mysql2/promise";
 export const executeSQL = async (SQL: string) => {
 	const connection = await mysql2.createConnection({
 		host: "localhost",
-		user: "admin",
+		user: "admin",//Nome de usuario cadastrado no banco: (caso não tenha feito - geralmente é o root)
 		password: "280819",
-		database: "Digital_Store",
+		database: "Digital_Store", //Nome do banco criado (tem que ser exatamente o mesmo nome)
 		port: 3306,
 	});
 
